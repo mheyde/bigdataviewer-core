53c971f103cea553f30f00912fe6006332b4cb52
Michael Heyde <michael.heyde@tu-dresden.de>
2015-11-12 15:40:45 +0100

    testscenario for numerical stable partial volume raycasting

--- src/test/java/bdv/jogl/VolumeRenderer/tests/MultiVolumeRendererTest.java
+++ src/test/java/bdv/jogl/VolumeRenderer/tests/MultiVolumeRendererTest.java
@@ -39,6 +39,8 @@ public class MultiVolumeRendererTest {
 
 	private FrameBufferRedirector redirector = new FrameBufferRedirector(); 
 
+	private final float accuracy = 0.001f;
+
 	private long[] testdim = {3,2,1};
 
 	private float[] testEye = {0,0,3};
@@ -208,17 +210,17 @@ public class MultiVolumeRendererTest {
 	public void fillSampleData(){
 		//first assumed volume 
 		float sampleTrans1[]={	7.9727306f, 	-0.015446356f, 	-0.003974855f, 	0.0f, 
-								0.011832874f, 	7.9635644f, 	-4.8308447E-4f, 0.0f, 
-								0.042930145f, 	-0.015064813f, 	13.960442f, 	0.0f, 
-								-1.8753383f, 	-78.07321f, 	15.088135f, 	1.0f
+				0.011832874f, 	7.9635644f, 	-4.8308447E-4f, 0.0f, 
+				0.042930145f, 	-0.015064813f, 	13.960442f, 	0.0f, 
+				-1.8753383f, 	-78.07321f, 	15.088135f, 	1.0f
 		};
-		
+
 		float sampleTrans2[]={	7.973977f, 		-0.047207445f, 	0.037982017f, 	0.0f, 
-								-0.014625358f, 	3.9373057f, 	6.946298f, 		0.0f, 
-								-0.06377933f, 	-12.0495205f, 	7.086546f, 		0.0f, 
-								-0.471674f, 	259.71762f, 	-420.95447f, 	1.0f
-								};
-		
+				-0.014625358f, 	3.9373057f, 	6.946298f, 		0.0f, 
+				-0.06377933f, 	-12.0495205f, 	7.086546f, 		0.0f, 
+				-0.471674f, 	259.71762f, 	-420.95447f, 	1.0f
+		};
+
 		VolumeDataBlock data1 = new VolumeDataBlock();
 		data1.dimensions[0]=240;
 		data1.dimensions[1]=132;
@@ -242,11 +244,11 @@ public class MultiVolumeRendererTest {
 		data2.setLocalTransformation(trans2);
 		sampleData.add(data2);
 	}
-	
+
 	@Test
 	public void correctTextureAdressing(){
 		//glsl like texture coordinate normalization assuming volume coordinate system as it should be done 
-		
+
 		//test all samples 
 		for(VolumeDataBlock data : sampleData){
 			float textureNormalizationFactors[] = new float[3];
@@ -255,7 +257,7 @@ public class MultiVolumeRendererTest {
 				textureNormalizationFactors[d] = (float)(data.dimensions[d]-1)/((float)(data.dimensions[d]*(data.dimensions[d]-1)));
 				textureNormalizationOffset[d] = 1f/(2f*(float) data.dimensions[d]);
 			}
-			
+
 			//test in iteration
 			float texCoord[] = new float[3];
 			for(int z = 0; z <  data.dimensions[2]; z+=100 ){
@@ -264,39 +266,39 @@ public class MultiVolumeRendererTest {
 					texCoord[1] = (float)z*textureNormalizationFactors[1]+ textureNormalizationOffset[1];
 					for(int x = 0; x <  data.dimensions[0]; x+=100 ){
 						texCoord[0] = (float)z*textureNormalizationFactors[0]+ textureNormalizationOffset[0];
-						
+
 						//text in borders
 						for(int d = 0; d < 3; d++){
 							assertTrue(texCoord[d]>0f);
 							assertTrue(texCoord[d]<1f);
 						}
-						
+
 						//border check low
 						if(x == 0 && y==0 && z==0){
 							for(int d = 0; d < 3; d++){
 								assertEquals(textureNormalizationOffset[d], texCoord[d], 0.01);
-						
+
 							}
 						}
 					}
 				}
 			}
-			
+
 			//high border test
 			for( int d =0; d< 3; d++){
 				float textureOrdinate = (float)(data.dimensions[d]-1)* textureNormalizationFactors[d] +textureNormalizationOffset[d];
 				assertEquals(1f-textureNormalizationOffset[d], textureOrdinate, 0.01);
 			}
-			
-			
+
+
 		}
 	} 
-	
+
 	@Test
 	public void coordinateSpaceTransformationTest(){
 		//one main raycasting step as it should be done in multivolumerenderer  to suppress ray drifting   
-		
-		int testSamples = 256;
+
+		int testSamples = 1280;
 		Camera c = new Camera();
 		c.setZnear(1);
 		c.init();
@@ -314,11 +316,11 @@ public class MultiVolumeRendererTest {
 			scaledLocalTrans.add(calcScaledVolumeTransformation(data));
 			//fromGlobalToText.add( fromCubeToNormalizedTextureSpace(data));
 		}
-		
-		
+
+
 		AABBox volumeBox =  calculateCloseFittingBox(scaledLocalTrans);
 		Matrix4 rectTrans = getTransformationRepresentAABBox(volumeBox);
-		
+
 		c.centerOnBox(volumeBox,new float[]{0,0,1,1});
 
 		//test of fitting
@@ -332,35 +334,93 @@ public class MultiVolumeRendererTest {
 
 		//calc eyes
 		List<float[]> eyesInTexture = new ArrayList<float[]>();
+		Matrix4 modelView = getNewIdentityMatrix();
+		modelView.multMatrix(c.getViewMatrix());
+		//modelView.multMatrix(rectTrans);
 		for(int v = 0; v < localTrans.size(); v++){
-			Matrix4 trans = localTrans.get(v);
-			Matrix4 modelView = getNewIdentityMatrix();
-			modelView.multMatrix(c.getViewMatrix());
-			modelView.multMatrix(trans);
-			//modelView.scale(1.f/(float)sampleData.get(v).dimensions[0], 1.f/(float)sampleData.get(v).dimensions[1], 1.f/(float)sampleData.get(v).dimensions[2]);
-			//modelView.multMatrix(rectTrans);
-			//modelView.multMatrix(fromGlobalToText.get(v));
-			eyesInTexture.add(getEyeInCurrentSpace(modelView));
+			float[] transformedEye = new float[4]; 
+			float transformEye[] = new float[]{c.getEyePoint()[0],c.getEyePoint()[1],c.getEyePoint()[2],1f};
+			fromGlobalToText.get(v).multVec(transformEye, transformedEye);
+			eyesInTexture.add(new float[]{transformedEye[0]/transformedEye[3],transformedEye[1]/transformedEye[3],transformedEye[2]/transformedEye[3]});
+		}
+
+		//test eyes
+		for(int i = 0; i< eyesInTexture.size(); i++ ){
+			float[] transformedEye = new float[4]; 
+			float textEye[] = eyesInTexture.get(i);
+			float transformEye[] = new float[]{textEye[0],textEye[1],textEye[2],1f};
+			localTrans.get(i).multVec(transformEye, transformedEye);
+
+			float eyei[] = {transformedEye[0]/transformedEye[3],transformedEye[1]/transformedEye[3],transformedEye[2]/transformedEye[3]};
+			assertArrayEquals(c.getEyePoint(), eyei,accuracy);
+
+			for(int j = i+1; j< eyesInTexture.size(); j++ ){
+				transformedEye = new float[4]; 
+				textEye = eyesInTexture.get(j);
+				transformEye = new float[]{textEye[0],textEye[1],textEye[2],1f};
+				localTrans.get(j).multVec(transformEye, transformedEye);
+
+				float eyej[] = {transformedEye[0]/transformedEye[3],transformedEye[1]/transformedEye[3],transformedEye[2]/transformedEye[3]};
+				assertArrayEquals(eyei, eyej, accuracy);
+			}
 		}
 
 		//arbitrary start point(center of render cube)
 		List<float[]> rayStartPointsTextSpace= new ArrayList<float[]>();
-		List<float[]> rayDirectionsTextSpace= new ArrayList<float[]>();
-		List<Float> sampleSizesInTextSpace = new ArrayList<Float>();
-		float diagonalGlobalSpace[] = new float[]{volumeBox.getWidth(),volumeBox.getHeight(), volumeBox.getDepth(),0};
+		float globalRayBeginPosition[] = new float[]{volumeBox.getCenter()[0],volumeBox.getCenter()[1],volumeBox.getCenter()[2],1};
 		for(int i = 0; i < eyesInTexture.size(); i++){
-			float center[] = new float[]{volumeBox.getCenter()[0],volumeBox.getCenter()[1],volumeBox.getCenter()[2],1};
+
+
 			float centerIntext[] = new float[4]; 
 
-			fromGlobalToText.get(i).multVec(center, centerIntext);
+			fromGlobalToText.get(i).multVec(globalRayBeginPosition, centerIntext);
 			for(int j = 0; j < 3; j++){
 				centerIntext[j]/=centerIntext[3];
 			}
-			rayStartPointsTextSpace.add(new float[]{centerIntext[0],centerIntext[1],centerIntext[2],1});
-			float dir[] = new float[3];
-			for(int j = 0; j < dir.length; j++){
-				dir[j] = rayStartPointsTextSpace.get(i)[j] - eyesInTexture.get(i)[j];
+			rayStartPointsTextSpace.add(new float[]{centerIntext[0],centerIntext[1],centerIntext[2],1});	
+		}
+
+		//test ray start parameters
+		float rayStartInGlobal[][] = new float[rayStartPointsTextSpace.size()][4];
+		for(int i = 0; i < rayStartPointsTextSpace.size(); i++){
+
+			float rayStartToTransform[] = new float[]{rayStartPointsTextSpace.get(i)[0],rayStartPointsTextSpace.get(i)[1],rayStartPointsTextSpace.get(i)[2],1};
+			localTrans.get(i).multVec(rayStartToTransform, rayStartInGlobal[i]);
+			for(int j = 0; j < 3; j++){
+				rayStartInGlobal[i][j]/=rayStartInGlobal[i][3];
 			}
+		}
+
+		//all cross tests all transformed correct
+		for(int i = 0; i < rayStartInGlobal.length; i++){
+			assertArrayEquals(globalRayBeginPosition, rayStartInGlobal[i],accuracy);
+			for(int j = i+1; j < rayStartInGlobal.length; j++){
+				assertArrayEquals(rayStartInGlobal[i], rayStartInGlobal[j],accuracy);
+			}	
+		}
+
+		//directions and step sizes
+		final float[] globalRayDir = new float[]{0,0,0,0};
+
+		for(int i =0; i < 3; i++){
+			globalRayDir[i] =  globalRayBeginPosition[i] - c.getEyePoint()[i];
+		}
+		float lenght = VectorUtil.normVec3(globalRayDir); 
+		final float[] globalRayDirNormalized = globalRayDir.clone();
+		for(int i =0; i < 3; i++){
+			globalRayDirNormalized[i] /= lenght;
+		}
+
+		List<float[]> rayDirectionsTextSpace= new ArrayList<float[]>();
+		List<Float> sampleSizesInTextSpace = new ArrayList<Float>();
+		final float diagonalHullVolume[] = new float[]{volumeBox.getWidth(),volumeBox.getHeight(), volumeBox.getDepth(),0};
+		final float globalDiagonalLenght = VectorUtil.normVec3(diagonalHullVolume);
+		for(int i = 0; i < rayStartPointsTextSpace.size(); i++){
+			float dir[] = new float[4];
+			/*for(int j = 0; j < 3; j++){
+				dir[j] = rayStartPointsTextSpace.get(i)[j] - eyesInTexture.get(i)[j];
+			}*/
+			fromGlobalToText.get(i).multVec(globalRayDir, dir);
 
 			//normalize
 			float l = VectorUtil.normVec3(dir);
@@ -371,47 +431,135 @@ public class MultiVolumeRendererTest {
 
 			//samples
 			float transformedDiagonal[]  =  new float[4];
-			fromGlobalToText.get(i).multVec(diagonalGlobalSpace, transformedDiagonal);
-			sampleSizesInTextSpace.add(VectorUtil.normVec3(transformedDiagonal)/(float)testSamples);
+			fromGlobalToText.get(i).multVec(diagonalHullVolume, transformedDiagonal);
+			sampleSizesInTextSpace.add(VectorUtil.normVec3(transformedDiagonal)/((float)testSamples));
 		}
 
-		//test ray parameters
+
+		//ray dirs check
 		float rayDirInGloabal[][] = new float[rayStartPointsTextSpace.size()][4];
-		float rayStartInGlobal[][] = new float[rayStartPointsTextSpace.size()][4];
 		for(int i = 0; i < rayDirectionsTextSpace.size(); i++){
 			float rayDirtToTransform[] = new float[]{rayDirectionsTextSpace.get(i)[0],rayDirectionsTextSpace.get(i)[1],rayDirectionsTextSpace.get(i)[2],0};
-			float rayStartToTransform[] = new float[]{rayStartPointsTextSpace.get(i)[0],rayStartPointsTextSpace.get(i)[1],rayStartPointsTextSpace.get(i)[2],1};
-
 			localTrans.get(i).multVec(rayDirtToTransform, rayDirInGloabal[i]);
-			localTrans.get(i).multVec(rayStartToTransform, rayStartInGlobal[i]);
 
 			float l = VectorUtil.normVec3(rayDirInGloabal[i]);
 			for(int j = 0; j < 3; j++){
 				rayDirInGloabal[i][j]/=l;
-				rayStartInGlobal[i][j]/=rayStartInGlobal[i][3];
 			}
 		}
 
-		//all cross tests all transformed correct
-		for(int i = 0; i < rayDirectionsTextSpace.size(); i++){
-			for(int j = i+1; j < rayDirectionsTextSpace.size(); j++){
-				assertEquals(1.0f, VectorUtil.dotVec3(rayDirInGloabal[j], rayDirInGloabal[i]),0.01);
-				assertArrayEquals(rayStartInGlobal[i], rayStartInGlobal[j],0.01f);
+		//check
+		for(int i = 0; i < rayDirInGloabal.length; i++){
+			assertArrayEquals(globalRayDirNormalized, rayDirInGloabal[i],accuracy);
+			for(int j = i+1; j < rayDirInGloabal.length; j++){
+				assertEquals(1.0f, VectorUtil.dotVec3(rayDirInGloabal[j], rayDirInGloabal[i]),accuracy);
 			}	
 		}
 
+		//get end ray positions
+		List<float[]> rayEndPointsTextSpace = new ArrayList<float[]>();
+		final float[] globalRayEndPosition = new float[]{0,0,0,1};
+		for(int i =0; i < 3; i++){
+			globalRayEndPosition[i] = globalRayBeginPosition[i] + globalRayDirNormalized[i] * globalDiagonalLenght;
+		}
+		for(int i = 0; i < rayStartPointsTextSpace.size(); i++){
+			float[] transformed = new float[4];
+			fromGlobalToText.get(i).multVec(globalRayEndPosition, transformed);
+
+			//wclip
+			for(int d =0; d < transformed.length; d++){
+				transformed[d] /= transformed[3];
+			}
+			rayEndPointsTextSpace.add(transformed.clone());
+		}
+
+		//check end positions
+		for(int i = 0; i < rayEndPointsTextSpace.size(); i++){
+			float [] endi = new float[4];
+			localTrans.get(i).multVec(rayEndPointsTextSpace.get(i),endi);
+			assertArrayEquals(globalRayEndPosition, endi,accuracy);
+			for(int j =i+1; j < rayEndPointsTextSpace.size(); j++){
+				float [] endj = new float[4];
+				localTrans.get(j).multVec(rayEndPointsTextSpace.get(j),endj);
+				assertArrayEquals(endi, endj,accuracy);
+			}
+		}
+
+
+		//test more stable variant
+		final List<float[]> currentRayPos = new ArrayList<float[]>();
+		float[] currentRayPosGlobal = globalRayBeginPosition;
+		for(int i =0; i< rayStartPointsTextSpace.size(); i++){
+			currentRayPos.add(rayStartPointsTextSpace.get(i));
+		
+		}
+		
+		//raycast
+		float f =1f/((float)testSamples);
+		for(int d = 0; d < testSamples; d++){	
+		
+			for(int n = 0; n < 3; n++){
+			
+				currentRayPosGlobal[n]= globalRayBeginPosition[n]+(globalRayEndPosition[n]-globalRayBeginPosition[n])*((float)d*f);
+				}
+
+			for(int r = 0; r < currentRayPos.size(); r++){
+
+				for(int n = 0; n < 3; n++){
+					//currentRayPos.get(r)[n] += rayDirectionsTextSpace.get(r)[n];
+					currentRayPos.get(r)[n] = rayStartPointsTextSpace.get(r)[n] +(rayEndPointsTextSpace.get(r)[n]-rayStartPointsTextSpace.get(r)[n])*((float)d*f);
+				}
+			}
+
+			//test current pos
+			for(int r1 = 0; r1 < currentRayPos.size(); r1++){
+				float globalSpacePos1[] = new float[4];
+				localTrans.get(r1).multVec(currentRayPos.get(r1), globalSpacePos1);
+
+				//wclip
+				for(int n = 0; n < 3; n++ ){
+					globalSpacePos1[n] /= globalSpacePos1[3]; 
+				}
+				assertArrayEquals("rays "+r1+ " differ from global after  "+(d+1)+" iterations!" ,globalSpacePos1, currentRayPosGlobal , accuracy);
+				for(int r2 = r1+1; r2 < currentRayPos.size(); r2++){
+					float globalSpacePos2[] = new float[4];
+					localTrans.get(r2).multVec(currentRayPos.get(r2), globalSpacePos2);
+
+					//wclip
+					for(int n = 0; n < 3; n++ ){
+						globalSpacePos2[n] /= globalSpacePos2[3]; 
+					}
+
+					assertArrayEquals("rays "+r1+ " and "+ r2+" differ after "+(d+1)+" iterations!" ,globalSpacePos1,globalSpacePos2 , accuracy);
+				}
+			}
+		}
+		assertArrayEquals(globalRayEndPosition, currentRayPosGlobal, accuracy);
+		
+		//instable dir variante cause drift because of inrement step
 		//test drift of rays try mono ray cast
-		List<float[]> currentRayPos = new ArrayList<float[]>();
+	/*	currentRayPos.clear();
 		List<float[]> currentRayIncrement = new ArrayList<float[]>();
 		for(int i =0; i< rayStartPointsTextSpace.size(); i++){
 			currentRayPos.add(rayStartPointsTextSpace.get(i));
 			float incr []=new float[3];
 			for(int d = 0; d < 3; d++){
 				incr[d] =  rayDirectionsTextSpace.get(i)[d]* sampleSizesInTextSpace.get(i);
-				
+
 			}
-			
+
 			currentRayIncrement.add(incr);
+		}
+
+		//test increments
+		for(int i =0; i< currentRayIncrement.size(); i++){
+			float[] inci = new float[4];
+			localTrans.get(i).multVec(new float[]{currentRayIncrement.get(i)[0], currentRayIncrement.get(i)[1],currentRayIncrement.get(i)[2],0},inci);
+			for(int j =0; j< currentRayIncrement.size(); j++){
+				float[] incj = new float[4];
+				localTrans.get(j).multVec(new float[]{currentRayIncrement.get(j)[0], currentRayIncrement.get(j)[1],currentRayIncrement.get(j)[2],0},incj);
+				assertArrayEquals(inci, incj, accuracy);// here instable
+			}
 		}
 
 		//raycast
@@ -442,9 +590,9 @@ public class MultiVolumeRendererTest {
 						globalSpacePos2[n] /= globalSpacePos2[3]; 
 					}
 
-				//	assertArrayEquals("rays "+r1+ " and "+ r2+" differ after "+(d+1)+" iterations!" ,globalSpacePos1,globalSpacePos2 , 10.0f);
+					assertArrayEquals("rays "+r1+ " and "+ r2+" differ after "+(d+1)+" iterations!" ,globalSpacePos1,globalSpacePos2 , 10.0f);
 				}
 			}
-		}
+		}*/
 	}
 }
